13a3d51ab3db5f8c1b23195c0e9f1d998dd88496
lericardolima <lericardolima@gmail.com>
2020-04-16 11:27:15 -0300

    Adicionar Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './config/router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Bootstrap configuration
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false
 
